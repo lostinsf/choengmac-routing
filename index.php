771c51d5368b4f4e3441f363c4857de7879f9c61
이봉주 <ibongju@ibongjuui-MacBookPro.local>
2022-03-08 08:01:26 +0900

    [Enhancement] main - [init] 추가 수정 확인 2차

--- index.php
+++ index.php
@@ -6,13 +6,13 @@ include_once ('config.php');
 <meta charset="utf-8"/>
 <meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1"/>
 <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no"/>
-<meta name="description" content="서면역2번출구, 1:1 전문의진료, 하지정맥류, LAST치료법, 비수술적치료, 의료보험수술, 마취과 전문의 상주"/>
-<meta name="keywords" content="서면역2번출구, 1:1 전문의진료, 하지정맥류, LAST치료법, 비수술적치료, 의료보험수술, 마취과 전문의 상주"/>
+<meta name="description" content="1:1 전문의진료, 하지정맥류, LAST치료법, 비수술적치료, 의료보험수술, 마취과 전문의 상주"/>
+<meta name="keywords" content="1:1 전문의진료, 하지정맥류, LAST치료법, 비수술적치료, 의료보험수술, 마취과 전문의 상주"/>
 <meta property="og:title" content="청맥병원"/>
 <meta property="og:type" content="website"/>
-<meta property="og:image" content="https://cheongmac.blob.core.windows.net/common/img_sns_og_img.png"/>
-<meta property="og:description" content="서면역2번출구, 1:1 전문의진료, 하지정맥류, LAST치료법, 비수술적치료, 의료보험수술, 마취과 전문의 상주"/>
-<title>청맥병원 | 청맥</title>
+<meta property="og:image" content=""/>
+<meta property="og:description" content="1:1 전문의진료, 하지정맥류, LAST치료법, 비수술적치료, 의료보험수술, 마취과 전문의 상주"/>
+<title>청맥병원</title>
 
 <link href="<?=ST_MAIN_URL?>/ic_main_favicon.ico" rel="icon">
 
@@ -32,32 +32,32 @@ include_once ('config.php');
             </a>
         </header>
         <div class="content">
-            <div class="menus">
-                <div class="item instagram" sns_type="instagram" onclick="sns_view('instagram')">
-                    바로보기
-                </div>
-                <div class="item youtube" sns_type="youtube" onclick="sns_view('youtube')">
-                    바로보기
-                </div>
-                <div class="item blog" sns_type="blog" onclick="sns_view('blog')">
-                    바로보기
-                </div>
-            </div>
+            <img src="https://cheongmac.blob.core.windows.net/common/bg_intro_routing.png" border="0" usemap="#Map">
+            <map name="Map" id="Map">
+                <area target="_blank" shape="rect" coords="97,576,276,635" href="https://혈관.com/" >
+                <area target="_blank" shape="rect" coords="696,576,874,633" href="http://울산하지정맥류.com">
+                <area target="_blank" shape="rect" coords="1120,574,1308,631" href="http://대구정맥류.com/" >
+            </map>
         </div>
     </div>
     <!-- MOBILE -->
     <div class="m_layout">
-        <div class="title">
-            <h2>스마트한<strong>의료생활</strong></h2>
-            <h1 id="setTitle">새롭게 그려내는</h1>
-            <h1 id="setSubject">청맥병원 <span class="light red">S</span><span class="light yellow">N</span><span class="light green">S</span></h1>
-        </div>
+        <header>
+            <a href="https://blog.naver.com/hiphoppoemic">
+                <img src="<?=ST_COMMON_URL?>/gif_intro_blog.gif">
+            </a>
+            <a href="https://www.facebook.com/yeshazi">
+                <img src="<?=ST_COMMON_URL?>/gif_intro_facebook.gif">
+            </a>
+        </header>
         <div class="content">
-            <div class="item youtube" onclick="sns_view('youtube')"><img src="<?=ST_COMMON_URL?>/img_sns_m_thumbnail1.png"></div>
-            <div class="item instagram" onclick="sns_view('instagram')"><img src="<?=ST_COMMON_URL?>/img_sns_m_thumbnail2.png"></div>
-            <div class="item blog" onclick="sns_view('blog')"><img src="<?=ST_COMMON_URL?>/img_sns_m_thumbnail3.png"></div>
+            <img src="https://cheongmac.blob.core.windows.net/common/bg_intro_routing_m.png" border="0" usemap="#Map1">
+            <map name="Map1" id="Map1">
+                <area target="_blank" shape="rect" coords="97,576,276,635" href="https://혈관.com/" >
+                <area target="_blank" shape="rect" coords="696,576,874,633" href="http://울산하지정맥류.com">
+                <area target="_blank" shape="rect" coords="1120,574,1308,631" href="http://대구정맥류.com/" >
+            </map>
         </div>
-        <a href="https://www.yeshazi.com" class="logo"><img src="<?=ST_COMMON_URL?>/ic_header_logo.svg"></a>
     </div>
 </section>
 
